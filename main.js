641952bd75d13d766a8911027f1b599a20390ab8
newmankr <fabricionewman@gmail.com>
2018-10-20 23:10:48 +0000

    variable update

--- main.js
+++ main.js
@@ -1,22 +1,23 @@
+var character = { x: 250, y: 300};
+
 function setup() {
   createCanvas(800,500);
 }
-var x = 250;
-var y = 300;
+
 function draw() {
-  //drawing the scenario - 1
+  //drawing the scenario
   background(40);
-  ellipse(x, y, 80, 80);
+  ellipse(character.x, character.y, 80, 80);
   rect(450,280,50,50);
 
-  //using the keyboard to control the character - 2
+  //using the keyboard to control the character
   if (keyIsDown(LEFT_ARROW) || keyIsDown(65)){
-    x-=10;
+    character.x-=10;
   } else if (keyIsDown(RIGHT_ARROW) || keyIsDown(68)){
-    x+=10;
+    character.x+=10;
   } else if (keyIsDown(UP_ARROW) || keyIsDown(87)){
-    y-=10;
+    character.y-=10;
   } else if (keyIsDown(DOWN_ARROW) || keyIsDown(83)){
-    y+=10;
+    character.y+=10;
   }
 }
