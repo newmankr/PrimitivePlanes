731d7bd7e76d0a77fffd9853f55f0e4bee3e58c2
newmankr <40243736+newmankr@users.noreply.github.com>
2018-10-20 14:15:32 +0000

    added keyboard input

--- main.js
+++ main.js
@@ -1,16 +1,22 @@
 function setup() {
   createCanvas(800,500);
 }
-var x = 300;
+var x = 250;
 var y = 300;
 function draw() {
+  //drawing the scenario
   background(40);
   ellipse(x, y, 80, 80);
-  rect(400,280,50,50);
+  rect(450,280,50,50);
 
-  if (keyIsDown(LEFT_ARROW)){
-    x = x - 10;
-  } else if (keyIsDown(RIGHT_ARROW)){
-    x = x + 10;
+  //using the keyboard to control the character
+  if (keyIsDown(LEFT_ARROW) || keyIsDown(65)){
+    x-=10;
+  } else if (keyIsDown(RIGHT_ARROW) || keyIsDown(68)){
+    x+=10;
+  } else if (keyIsDown(UP_ARROW) || keyIsDown(87)){
+    y-=10;
+  } else if (keyIsDown(DOWN_ARROW) || keyIsDown(83)){
+    y+=10;
   }
 }
