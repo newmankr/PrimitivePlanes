c54aff68c38aaf9da13a10a9e57c3e1b76d02f75
newmankr <40243736+newmankr@users.noreply.github.com>
2018-10-27 18:00:39 +0000

    Create etapa2.js

---
+++ etapas/etapa2.js
@@ -0,0 +1,22 @@
+var character = { x: 250, y: 300};
+
+function setup() {
+  createCanvas(800,500);
+}
+
+function draw() {
+  //drawing the scenario
+  background(40);
+  ellipse(character.x, character.y, 80, 80);
+
+  //using the keyboard to control the character
+  if (keyIsDown(LEFT_ARROW) || keyIsDown(65)){
+    character.x-=10;
+  } else if (keyIsDown(RIGHT_ARROW) || keyIsDown(68)){
+    character.x+=10;
+  } else if (keyIsDown(UP_ARROW) || keyIsDown(87)){
+    character.y-=10;
+  } else if (keyIsDown(DOWN_ARROW) || keyIsDown(83)){
+    character.y+=10;
+  }
+}
